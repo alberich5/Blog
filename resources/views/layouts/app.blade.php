d1ea848c2ca8fd86819485899fd4aecf01185e69
omar zarate <alberich_sid@hotmail.com>
2019-01-14 18:10:57 -0600

    cmabios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Omar Zarate') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -48,7 +48,7 @@
                         @else
                             <li><a href="{{ route('tags.index') }}">Etiquetas</a></li>
                             <li><a href="{{ route('categories.index') }}">Categorías</a></li>
-                            <li><a href="{{ route('posts.index') }}">Entradas</a></li>                            
+                            <li><a href="{{ route('posts.index') }}">Entradas</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -86,7 +86,7 @@
             </div>
         @endif
 
-        @if(count($errors))            
+        @if(count($errors))
             <div class="container">
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2">
@@ -103,7 +103,7 @@
         @endif
 
         @yield('content')
-        
+
     </div>
 
     <!-- Scripts -->
